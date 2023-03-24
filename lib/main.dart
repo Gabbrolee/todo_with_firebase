d03e6b574d5ec77dfb6b93fb7a425fd20432b495
Ameh Christopher <akrine2christophine@gmail.com>
2023-03-24 11:16:57 +0100

    complete a simple todo app using firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'dart:io';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -13,12 +19,9 @@ class MyApp extends StatelessWidget {
         title: 'Todo with Firebase',
         theme: ThemeData(
             colorScheme: ColorScheme.fromSwatch(
-                primarySwatch: Colors.blue,
-                brightness: Brightness.light
-            )
+                    primarySwatch: Colors.blue, brightness: Brightness.light)
                 .copyWith(secondary: Colors.orange)),
-        home: Home()
-    );
+        home: Home());
   }
 }
 
@@ -31,80 +34,93 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List todos = [];
-  String input = "";
+  String todoTitle = "";
 
-  @override
-  void initState() {
-    todos.add("item1");
-    todos.add("item2");
-    todos.add("item3");
-    todos.add("item4");
-    super.initState();
+  createTodo() {
+    DocumentReference documentReference =
+        FirebaseFirestore.instance.collection("MyTodos").doc(todoTitle);
+
+    Map<String, String> todos = {"todoTitle": todoTitle};
+    documentReference.set(todos).whenComplete(() {
+      print("todos created");
+    });
+  }
+
+  deleteTodo(item) {
+    DocumentReference documentReference =
+    FirebaseFirestore.instance.collection("MyTodos").doc(item);
+    documentReference.delete().whenComplete(() {
+      print("$item todos deleted");
+    });
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Todos"),
-        centerTitle: true,
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: (){
-          showDialog(
-              context: context,
-              builder: (BuildContext context){
-                return AlertDialog(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0)
-                  ),
-                  title: const Text("Add todo list"),
-                  content: TextField(
-                    onChanged: (String value){
-                      input = value;
-                    },
-                  ),
-                  actions: [
-                    TextButton(
-                      child: const Text("Add"),
-                      onPressed: (){
-                       setState(() {
-                         todos.add(input);
-                       });
-                       Navigator.pop(context);
-                      },
-                    )
-                  ],
-                );
-              });
-        },
-      ),
-      body: ListView.builder(
-          itemCount: todos.length,
-          itemBuilder: (BuildContext context, index){
-            return Dismissible(
-                key: Key(todos[index]),
-                child: Card(
-                  margin: const EdgeInsets.all(8),
-                  elevation: 4.0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0)
-                  ),
-                  child: ListTile(
-                    title: Text(todos[index]),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.delete,  color: Colors.red),
-                      onPressed: (){
-                      setState(() {
-                        todos.remove(todos[index]);
-                      });
+        appBar: AppBar(
+          title: const Text("Todos"),
+          centerTitle: true,
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0)),
+                    title: const Text("Add todo list"),
+                    content: TextField(
+                      onChanged: (String value) {
+                        todoTitle = value;
                       },
                     ),
-                  ),
-                )
-            );
-          }
-      ),
-    );
+                    actions: [
+                      TextButton(
+                        child: const Text("Add"),
+                        onPressed: () {
+                          // setState(() {
+                          //   todos.add(input);
+                          // });
+                          createTodo();
+                          Navigator.pop(context);
+                        },
+                      )
+                    ],
+                  );
+                });
+          },
+        ),
+        body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection("MyTodos").snapshots(),
+          builder: (context, snapshot) {
+            return ListView.builder(
+                shrinkWrap: true,
+                itemCount: snapshot.data?.docs.length,
+                itemBuilder: (context, index) {
+                  DocumentSnapshot documentSnapshot = snapshot.data!.docs[index];
+                  return Dismissible(
+                    onDismissed: (direction){
+                      deleteTodo("todoTitle");
+                    },
+                      key: Key(documentSnapshot["todoTitle"]),
+                      child: Card(
+                        margin: const EdgeInsets.all(8),
+                        elevation: 4.0,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0)),
+                        child: ListTile(
+                          title: Text(documentSnapshot['todoTitle']),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.delete, color: Colors.red),
+                            onPressed: () {
+                              deleteTodo(documentSnapshot["todoTitle"]);
+                            },
+                          ),
+                        ),
+                      ));
+                });
+          },
+        ));
   }
 }
